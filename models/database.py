6eddc2c5896f0b7643633e3ef3cb4ea0cbaea35d
Augusto Gil de Oliveira <augustogildeoliveira@gmail.com>
2020-05-20 22:21:08 -0300

    Enxugamento de código e adição da possibilidade de inserir timestamp em segundos

--- models/database.py
+++ models/database.py
@@ -1,9 +1,6 @@
-from influxdb_client import InfluxDBClient, Point, WritePrecision
+from influxdb_client import InfluxDBClient, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
-
-
-
-#Suportar de inicio apenas temperatura
+from influxdb_client.rest import ApiException
 
 class DatabaseInteract():
 
@@ -16,15 +13,14 @@ class DatabaseInteract():
   def insert(self):
     client = InfluxDBClient(url="https://us-central1-1.gcp.cloud2.influxdata.com",token=self.token) # Conceta
     write_api = client.write_api(write_options=SYNCHRONOUS) # Metodo write
-    print(self.data)
-    write_api.write(self.bucket,self.org,self.data)# Insere o dado
-
-    query = f'from(bucket: \"{self.bucket}\") |> range(start: -5s)' # Query de consulta
-    tables = client.query_api().query(query, org=self.org) #Executa a query
-
-    result = []
-    for table in tables:
-      for row in table.records:
-        print(f'{row.values["_time"]}, {row.values["device_id"]}, {row.values["_field"]}, {row.values["_value"]}')
-        result.append(str(row))
-    return result
+    
+    try:
+      # write_precision padrão é nano segundos, setado para segundos
+      write_api.write(self.bucket,self.org,self.data,write_precision=WritePrecision.S)
+    except ApiException as e:
+        print(e)
+        return {"message": "An error ocurred in the server-side"},500
+    write_api.__del__()
+    client.__del__()
+    
+    return {"message": "Created"},201
