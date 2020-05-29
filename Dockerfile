d6d580753d7f2208a73c0ba830c522cdfcf2d9ff
Augusto Oliveira <augustogildeoliveira@gmail.com>
2020-05-29 16:17:35 +0000

    Agora os dados para conexão no InfluxDB são passados por variáveis de ambiente do SO

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,10 @@ COPY . /code
 WORKDIR /code
 ENV FLASK_APP app.py
 ENV FLASK_RUN_HOST 0.0.0.0
+ENV INFLUXDB_V2_URL 127.0.0.1
+ENV INFLUXDB_V2_ORG org
+ENV INFLUXDB_V2_TOKEN token
+ENV INFLUXDB_V2_BUCKET bucket
 RUN pip install --no-cache-dir -r requirements.txt
 EXPOSE 5000
 CMD flask run
--- models/database.py
+++ models/database.py
@@ -1,3 +1,4 @@
+from os import getenv
 from influxdb_client import InfluxDBClient, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 from influxdb_client.rest import ApiException
@@ -6,21 +7,18 @@ class DatabaseInteract():
 
   def __init__(self,data):
       self.data = data
-      self.token = ''
-      self.bucket = ''
-      self.org = ''
-
+      self.bucket = getenv('INFLUXDB_V2_BUCKET')
   def insert(self):
-    client = InfluxDBClient(url="https://us-central1-1.gcp.cloud2.influxdata.com",token=self.token) # Conceta
-    write_api = client.write_api(write_options=SYNCHRONOUS) # Metodo write
-    
+    client = InfluxDBClient.from_env_properties()
+    write_api = client.write_api(write_options=SYNCHRONOUS)
     try:
-      # write_precision padrão é nano segundos, setado para segundos
-      write_api.write(self.bucket,self.org,self.data,write_precision=WritePrecision.S)
+      print(self.bucket)
+      # write_precision padrão é nanosegundos, setado para segundo
+      write_api.write(self.bucket,client.org,self.data,write_precision=WritePrecision.S)
     except ApiException as e:
         print(e)
         return {"message": "An error ocurred in the server-side"},500
     write_api.__del__()
     client.__del__()
-    
+
     return {"message": "Created"},201
